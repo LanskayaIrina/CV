3c4bda5397900d139eb64bf4aa93fb9a40ab4b2b
Irina Lanskaia <irina.lanskaia@nixsolutions.com>
2020-08-06 10:40:24 +0300

    rename base_url

--- src/constants/pathNames.js
+++ src/constants/pathNames.js
@@ -1,4 +1,4 @@
-export const BASE_URL = '/cv/';
+export const BASE_URL = '/Curriculum_Vitae/';
 export const SKILLS = `${BASE_URL}skills`;
 export const COURSES = `${BASE_URL}courses`;
 export const INTERESTS = `${BASE_URL}interests`;

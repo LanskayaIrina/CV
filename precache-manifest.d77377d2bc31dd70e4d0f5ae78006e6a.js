b93035f189e9be16c95a7ccb1269737ac519db62
Irina Lanskaia <irina.lanskaia@nixsolutions.com>
2020-08-14 18:26:05 +0300

    Updates

---
+++ precache-manifest.d77377d2bc31dd70e4d0f5ae78006e6a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c999f9975d180b8fa24efb9bee3136",
+    "url": "/CV_Irina_Lanskaya/index.html"
+  },
+  {
+    "revision": "31a0dd706e58efd139f6",
+    "url": "/CV_Irina_Lanskaya/static/css/main.bd013663.chunk.css"
+  },
+  {
+    "revision": "55afb24468707ce13480",
+    "url": "/CV_Irina_Lanskaya/static/js/2.79614065.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CV_Irina_Lanskaya/static/js/2.79614065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a0dd706e58efd139f6",
+    "url": "/CV_Irina_Lanskaya/static/js/main.3482a91f.chunk.js"
+  },
+  {
+    "revision": "20ac78f00f24db741823",
+    "url": "/CV_Irina_Lanskaya/static/js/runtime-main.ae2a3055.js"
+  },
+  {
+    "revision": "9b2bd82e4c6005a65afdd07d96122e9f",
+    "url": "/CV_Irina_Lanskaya/static/media/4.9b2bd82e.png"
+  },
+  {
+    "revision": "110581fc18d72ab163b35d11abb1c565",
+    "url": "/CV_Irina_Lanskaya/static/media/Boock.110581fc.png"
+  },
+  {
+    "revision": "dc82f742547f1039926b1be7fa238456",
+    "url": "/CV_Irina_Lanskaya/static/media/ColoredCrayons-7wPR.dc82f742.ttf"
+  },
+  {
+    "revision": "130ba5ab7ea26fd9f855b14551f2e2b4",
+    "url": "/CV_Irina_Lanskaya/static/media/Cup.130ba5ab.png"
+  },
+  {
+    "revision": "773b2fcb511189de474c6203ae7fedb4",
+    "url": "/CV_Irina_Lanskaya/static/media/JacketFirst.773b2fcb.png"
+  },
+  {
+    "revision": "38dc76380f78fa1af77f6b195a9963cf",
+    "url": "/CV_Irina_Lanskaya/static/media/Pen.38dc7638.png"
+  },
+  {
+    "revision": "dbe57d64401dfcd726bd28d381dad209",
+    "url": "/CV_Irina_Lanskaya/static/media/SchistRegular.dbe57d64.ttf"
+  },
+  {
+    "revision": "b8153a79fec16e7163dfc83b7cc34dfa",
+    "url": "/CV_Irina_Lanskaya/static/media/SemenFirst.b8153a79.png"
+  },
+  {
+    "revision": "56313fd7d53c758a928d262fdd9b9d3c",
+    "url": "/CV_Irina_Lanskaya/static/media/SemenGif.56313fd7.gif"
+  },
+  {
+    "revision": "8a0be942077b62d96ac0c849b6006f11",
+    "url": "/CV_Irina_Lanskaya/static/media/Slide1.8a0be942.png"
+  },
+  {
+    "revision": "b69f629f9a37d704807811c500481303",
+    "url": "/CV_Irina_Lanskaya/static/media/Slide2.0.b69f629f.png"
+  },
+  {
+    "revision": "99be281263ef0d5b6814c94b607f0367",
+    "url": "/CV_Irina_Lanskaya/static/media/Slide2.1.99be2812.png"
+  },
+  {
+    "revision": "5ace567377fe8bbda4df693784dd646d",
+    "url": "/CV_Irina_Lanskaya/static/media/Slide3.0.5ace5673.png"
+  },
+  {
+    "revision": "c43b07bbabce69309418bd9233c8caf3",
+    "url": "/CV_Irina_Lanskaya/static/media/Slide3.2.c43b07bb.png"
+  },
+  {
+    "revision": "67afad35bf986384b3b74d0643182cb0",
+    "url": "/CV_Irina_Lanskaya/static/media/StickerCss.67afad35.png"
+  },
+  {
+    "revision": "592c4db21f4f2a9d4e7d79f0fc1a9f8b",
+    "url": "/CV_Irina_Lanskaya/static/media/StickerHtml.592c4db2.png"
+  },
+  {
+    "revision": "75be419d0c529ac0cd4f8efec688f9d0",
+    "url": "/CV_Irina_Lanskaya/static/media/StickerJs.75be419d.png"
+  },
+  {
+    "revision": "5decf596cb68309190656344054948c7",
+    "url": "/CV_Irina_Lanskaya/static/media/StickerOther.5decf596.png"
+  },
+  {
+    "revision": "7d87008bdc3bc46689905de2969286a3",
+    "url": "/CV_Irina_Lanskaya/static/media/StickerReact.7d87008b.png"
+  },
+  {
+    "revision": "b84d8562f94d68ec7007552dc4a0004a",
+    "url": "/CV_Irina_Lanskaya/static/media/background.b84d8562.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Curriculum_Vitae/precache-manifest.063cac0d0d7a8f6ddb0cc315c0000b8f.js"
+  "/CV_Irina_Lanskaya/precache-manifest.d77377d2bc31dd70e4d0f5ae78006e6a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Curriculum_Vitae/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CV_Irina_Lanskaya/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
